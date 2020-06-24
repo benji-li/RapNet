1401c651cec6ce950e66c28c6a70256ceb3b55eb
benjelloo <55926840+benjelloo@users.noreply.github.com>
2020-06-24 12:05:40 -0700

    implemented training_loop

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ def fb_props(rnn,optimizer,criterion,inp,target,hidden,gpu_avail):
         inp,target = inp.cuda(),target.cuda()
     
     hidden = tuple([x.data for x in hidden])
-    
+
     output,hidden = rnn.forward(inp,hidden)
     loss = criterion(output.squeeze(),target)
     loss.backward()
@@ -22,4 +22,23 @@ def fb_props(rnn,optimizer,criterion,inp,target,hidden,gpu_avail):
 
 
 def training_loop(rnn,batch_size,optimizer,criterion,n_epochs):
-    pass
+    gpu_avail=False
+    if torch.cuda_is_available():
+        rnn.cuda()
+        gpu_avail=True
+    losses=[]
+    rnn.train()
+    for epoch in range(n_epochs):
+        hidden = rnn.init_hidden(batch_size,gpu_avail)
+        for batch, (inputs,targets) in enumerate(train_loader,1):
+            #ensure it's a full batch before props
+            n_batches = len(train_loader.dataset)//batch_size
+            if(batch > n_batches):
+                break
+            loss,hidden = fb_props(rnn,optimizer,criterion,inputs,targets,hidden,gpu_avail)
+            losses.append(loss)
+            
+            if batch%100==0:
+                print('Epoch {}/{} \t Loss: {}'.format(epoch+1,n_epochs,np.average(losses)))
+                losses=[]
+    return rnn
