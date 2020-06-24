104f54e065898c66e6494b8cd5271f5caf118351
benjelloo <55926840+benjelloo@users.noreply.github.com>
2020-06-23 21:22:40 -0700

    initialized functions

---
+++ train.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import dataprep
+import model
+
+def fb_props(rnn,optimizer,criterion,inp,target,hidden):
+    pass
+
+def training_loop(rnn,batch_size,optimizer,criterion,n_epochs):
+    pass
