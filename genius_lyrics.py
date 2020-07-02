6e4dbf4fc6309af4b0a8c1ef07410fa02c9a44c6
benjelloo <55926840+benjelloo@users.noreply.github.com>
2020-07-01 18:48:46 -0700

    scrapes Genius to create lyric databases

---
+++ genius_lyrics.py
@@ -0,0 +1,24 @@
+import lyricsgenius as genius
+import json
+import os
+
+api = genius.Genius('4eMJoKWuTAyfxabfE_CZekpjNgp1JBTD86ULdSC3JfUGmHnLrz853VKif2pLMZdE')
+
+artist_name = 'KendrickLamar'
+artist = api.search_artist(artist_name,max_songs=200)
+
+artist.save_lyrics()
+
+with open('Lyrics_{}.json'.format(artist_name)) as json_data:
+    data = json.load(json_data)
+os.remove('./Lyrics_{}.json'.format(artist_name))
+
+lyrics=[]
+for song in data['songs']:
+    lyrics.append(song['lyrics'])
+
+filepath = "./data/"+artist_name+"_Lyrics.txt"
+
+f = open(filepath,'w')
+f.write(str(lyrics))
+f.close()
